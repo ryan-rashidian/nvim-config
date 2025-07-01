d9a608559c3a3ded529bce9a5dc78d7068b2a5aa
ryan-rashidian <ryanrashidian@gmail.com>
2025-06-30 17:47:37 -0700

    FIX nvim-tree plugin show git ignore files by default

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,29 +1,32 @@
 -- nvim/lua/plugins/nvimtree.lua
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("nvim-tree").setup({
-      view = {
-        width = 30,
-        side = "left",
-      },
-      renderer = {
-        icons = {
-          show = {
-            git = true,
-            folder = true,
-            file = true,
-            folder_arrow = true,
-          },
-        },
-      },
-      filters = {
-        dotfiles = false,
-      },
-    })
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("nvim-tree").setup({
+            view = {
+                width = 30,
+                side = "left",
+            },
+            renderer = {
+                icons = {
+                    show = {
+                        git = true,
+                        folder = true,
+                        file = true,
+                        folder_arrow = true,
+                    },
+                },
+            },
+            filters = {
+                dotfiles = false,
+            },
+            git = {
+                ignore = false,
+            },
+        })
 
-    -- optional keymap
-  end,
+        -- optional keymap
+    end,
 }
 
