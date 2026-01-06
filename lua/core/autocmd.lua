f6d288e98248cda60a4aabb32b10aeb9ed923feb
ryan-rashidian <ryanrashidian@gmail.com>
2026-01-06 12:03:53 -0800

    Update autocmd settings, remove all except spell-checker

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,17 +1,8 @@
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "markdown", "text", "gitcommit" },
+    pattern = { "markdown", "text" },
     callback = function()
         vim.opt_local.spell = true
         vim.opt_local.spelllang = { "en_us" }
-
-        vim.opt_local.wrap = true
-        vim.opt_local.linebreak = true
-        vim.opt_local.showbreak = "↪ "
-        vim.opt_local.conceallevel = 2
-        vim.opt_local.formatoptions:append("n")
-        vim.opt_local.textwidth = 80
-        vim.opt_local.colorcolumn = "80"
-        vim.opt_local.swapfile = false
     end,
 })
 
