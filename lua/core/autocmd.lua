a2ec18a2bf6749eead5dbeb1caf5edff413b6c9d
ryan-rashidian <ryanrashidian@gmail.com>
2026-01-11 11:37:37 -0800

    Add line-wrapping for markdown and text files

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -3,6 +3,9 @@ vim.api.nvim_create_autocmd("FileType", {
     callback = function()
         vim.opt_local.spell = true
         vim.opt_local.spelllang = { "en_us" }
+        vim.opt_local.wrap = true
+        vim.opt_local.linebreak = true
+        vim.opt_local.breakindent = true
     end,
 })
 
